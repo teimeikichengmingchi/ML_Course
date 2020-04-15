2714faa444cfdf70e39d2afd3b7dc037fad15ed6
teimeikichengmingchi <59116228+teimeikichengmingchi@users.noreply.github.com>
2020-04-15 21:22:14 +0800

    Add files via upload

--- ml_play.py
+++ ml_play.py
@@ -21,7 +21,10 @@ def ml_loop():
 
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
-    ball_served = False
+    
+    #Rmove = False
+    Lmove = False
+    s = [93, 93]
 
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
@@ -30,23 +33,41 @@ def ml_loop():
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
+        rate = [scene_info.ball[0] - s[0], scene_info.ball[1] - s[1]]
 
         # 3.2. If the game is over or passed, the game process will reset
         #      the scene and wait for ml process doing resetting job.
         if scene_info.status == GameStatus.GAME_OVER or \
             scene_info.status == GameStatus.GAME_PASS:
             # Do some stuff if needed
-            ball_served = False
 
             # 3.2.1. Inform the game process that ml process is ready
             comm.ml_ready()
             continue
 
         # 3.3. Put the code here to handle the scene information
+        if scene_info.ball[1] > 170:
+            if  rate[0] > 0 and \
+                (scene_info.ball[0] + 400 - scene_info.ball[1] > scene_info.platform[0] + 20):
+                #Rmove = True
+                Lmove = False
+            elif rate[0] < 0 and \
+                (scene_info.ball[0] + 400 - scene_info.ball[1] < scene_info.platform[0]):
+                Lmove = True
+                #Rmove = False
+        else:
+            if scene_info.platform[0] + 20 < 100:
+                #Rmove = True
+                Lmove = False
+            else:
+                Lmove = True
+                #Rmove = False
+
+        s = scene_info.ball
+                    
 
         # 3.4. Send the instruction for this frame to the game process
-        if not ball_served:
+        if Lmove:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
-            ball_served = True
         else:
             comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
